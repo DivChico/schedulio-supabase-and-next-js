4fd0350569300f32fdd0d3afa150f15d4c855016
Abdullah Hamdy <abdullah2001hamdy@gmail.com>
2024-09-14 19:14:25 +0300

    fullcalender

--- app/(dashboard)/(routes)/schedule/page.jsx
+++ app/(dashboard)/(routes)/schedule/page.jsx
@@ -1,12 +1,7 @@
 "use client";
 import React from "react";
-import { Paper } from "@mui/material";
-import {
-  Appointments,
-  DayView,
-  Scheduler,
-  ViewState,
-} from "@devexpress/dx-react-scheduler";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 const Page = () => {
   // Get today's date in YYYY-MM-DD format
@@ -15,78 +10,217 @@ const Page = () => {
 
   const dailySchedule = [
     {
-      startDate: new Date(`${currentDate}T05:30:00`),
-      endDate: new Date(`${currentDate}T06:00:00`),
+      id: "1",
+      groupId: "group1",
+      allDay: false,
+      start: new Date(2024, 8, 14, 5, 30),
+      startStr: new Date(2024, 8, 14, 5, 30).toISOString(),
+      end: new Date(2024, 8, 14, 6, 0),
+      endStr: new Date(2024, 8, 14, 6, 0).toISOString(),
       title: "Wake up and Morning Routine",
-      allDay: false,
-      id: 1,
+      editable: true,
+      startEditable: true,
+      durationEditable: true,
+      resourceEditable: true,
+      overlap: true,
+      display: "block",
+      backgroundColor: "#FFCC00",
+      borderColor: "#FFCC00",
+      textColor: "#000000",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
     {
-      startDate: new Date(`${currentDate}T06:00:00`),
-      endDate: new Date(`${currentDate}T06:20:00`),
+      id: "2",
+      groupId: "group1",
+      allDay: false,
+      start: new Date(2024, 8, 14, 6, 0),
+      startStr: new Date(2024, 8, 14, 6, 0).toISOString(),
+      end: new Date(2024, 8, 14, 6, 20),
+      endStr: new Date(2024, 8, 14, 6, 20).toISOString(),
       title: "Fill Water Tank",
-      allDay: false,
-      id: 2,
+      editable: true,
+      startEditable: true,
+      durationEditable: true,
+      resourceEditable: true,
+      overlap: true,
+      display: "block",
+      backgroundColor: "#00CCFF",
+      borderColor: "#00CCFF",
+      textColor: "#000000",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
     {
-      startDate: new Date(`${currentDate}T06:30:00`),
-      endDate: new Date(`${currentDate}T14:00:00`),
+      id: "3",
+      groupId: "group2",
+      allDay: false,
+      start: new Date(2024, 8, 14, 6, 30),
+      startStr: new Date(2024, 8, 14, 6, 30).toISOString(),
+      end: new Date(2024, 8, 14, 14, 0),
+      endStr: new Date(2024, 8, 14, 14, 0).toISOString(),
       title: "Work",
-      allDay: false,
-      id: 3,
-      rRule: "FREQ=DAILY;BYHOUR=6,7,8,9,10,11,12,13",
+      editable: false,
+      startEditable: false,
+      durationEditable: false,
+      resourceEditable: false,
+      overlap: false,
+      display: "block",
+      backgroundColor: "#FF5733",
+      borderColor: "#FF5733",
+      textColor: "#FFFFFF",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
     {
-      startDate: new Date(`${currentDate}T14:00:00`),
-      endDate: new Date(`${currentDate}T14:30:00`),
+      id: "4",
+      groupId: "group2",
+      allDay: false,
+      start: new Date(2024, 8, 14, 14, 0),
+      startStr: new Date(2024, 8, 14, 14, 0).toISOString(),
+      end: new Date(2024, 8, 14, 14, 30),
+      endStr: new Date(2024, 8, 14, 14, 30).toISOString(),
       title: "Lunch Break",
-      allDay: false,
-      id: 4,
+      editable: true,
+      startEditable: true,
+      durationEditable: true,
+      resourceEditable: true,
+      overlap: true,
+      display: "block",
+      backgroundColor: "#FFB6C1",
+      borderColor: "#FFB6C1",
+      textColor: "#000000",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
     {
-      startDate: new Date(`${currentDate}T15:00:00`),
-      endDate: new Date(`${currentDate}T17:00:00`),
+      id: "5",
+      groupId: "group3",
+      allDay: false,
+      start: new Date(2024, 8, 14, 15, 0),
+      startStr: new Date(2024, 8, 14, 15, 0).toISOString(),
+      end: new Date(2024, 8, 14, 17, 0),
+      endStr: new Date(2024, 8, 14, 17, 0).toISOString(),
       title: "Work on SaaS App",
-      allDay: false,
-      id: 5,
+      editable: true,
+      startEditable: true,
+      durationEditable: true,
+      resourceEditable: true,
+      overlap: true,
+      display: "block",
+      backgroundColor: "#3CB371",
+      borderColor: "#3CB371",
+      textColor: "#FFFFFF",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
     {
-      startDate: new Date(`${currentDate}T17:30:00`),
-      endDate: new Date(`${currentDate}T19:00:00`),
+      id: "6",
+      groupId: "group3",
+      allDay: false,
+      start: new Date(2024, 8, 14, 17, 30),
+      startStr: new Date(2024, 8, 14, 17, 30).toISOString(),
+      end: new Date(2024, 8, 14, 19, 0),
+      endStr: new Date(2024, 8, 14, 19, 0).toISOString(),
       title: "Work on YouTube Channel",
-      allDay: false,
-      id: 6,
+      editable: true,
+      startEditable: true,
+      durationEditable: true,
+      resourceEditable: true,
+      overlap: true,
+      display: "block",
+      backgroundColor: "#1E90FF",
+      borderColor: "#1E90FF",
+      textColor: "#FFFFFF",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
     {
-      startDate: new Date(`${currentDate}T19:30:00`),
-      endDate: new Date(`${currentDate}T21:00:00`),
+      id: "7",
+      groupId: "group4",
+      allDay: false,
+      start: new Date(2024, 8, 14, 19, 30),
+      startStr: new Date(2024, 8, 14, 19, 30).toISOString(),
+      end: new Date(2024, 8, 14, 21, 0),
+      endStr: new Date(2024, 8, 14, 21, 0).toISOString(),
       title: "Relaxation and Personal Time",
-      allDay: false,
-      id: 7,
+      editable: true,
+      startEditable: true,
+      durationEditable: true,
+      resourceEditable: true,
+      overlap: true,
+      display: "block",
+      backgroundColor: "#FFD700",
+      borderColor: "#FFD700",
+      textColor: "#000000",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
     {
-      startDate: new Date(`${currentDate}T21:30:00`),
-      endDate: new Date(`${currentDate}T23:00:00`),
+      id: "8",
+      groupId: "group4",
+      allDay: false,
+      start: new Date(2024, 8, 14, 21, 30),
+      startStr: new Date(2024, 8, 14, 21, 30).toISOString(),
+      end: new Date(2024, 8, 14, 23, 0),
+      endStr: new Date(2024, 8, 14, 23, 0).toISOString(),
       title: "Dinner and Wind Down",
-      allDay: false,
-      id: 8,
+      editable: true,
+      startEditable: true,
+      durationEditable: true,
+      resourceEditable: true,
+      overlap: true,
+      display: "block",
+      backgroundColor: "#FF4500",
+      borderColor: "#FF4500",
+      textColor: "#FFFFFF",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
     {
-      startDate: new Date(`${currentDate}T23:00:00`),
-      title: "Sleep",
+      id: "9",
+      groupId: "group5",
       allDay: true,
-      id: 9,
+      start: new Date(2024, 8, 14, 23, 0),
+      startStr: new Date(2024, 8, 14, 23, 0).toISOString(),
+      end: new Date(2024, 8, 15, 5, 30),
+      endStr: new Date(2024, 8, 15, 5, 30).toISOString(),
+      title: "Sleep",
+      editable: false,
+      startEditable: false,
+      durationEditable: false,
+      resourceEditable: false,
+      overlap: false,
+      display: "block",
+      backgroundColor: "#000080",
+      borderColor: "#000080",
+      textColor: "#FFFFFF",
+      extendedProps: {
+        recurring: "FREQ=DAILY",
+      },
     },
   ];
 
+  console.log(dailySchedule);
+
   return (
-    <Paper>
-      <Scheduler data={dailySchedule}>
-        <ViewState currentDate={currentDate} />
-        <DayView startDayHour={5} endDayHour={23} />
-        <Appointments />
-      </Scheduler>
-    </Paper>
+    <div>
+      <FullCalendar
+        allDaySlot={false}
+        plugins={[timeGridPlugin]}
+        initialView="timeGridDay"
+        weekends={true}
+        events={dailySchedule}
+      />
+    </div>
   );
 };
 
